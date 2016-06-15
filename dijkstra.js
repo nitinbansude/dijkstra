fe95ed211130ba0cfdf06220d125b57b4e1a8e2d
Riandy Rahman Nugraha <riandyrn@gmail.com>
2016-06-15 16:18:06 +0700

    add return path

--- dijkstra.js
+++ dijkstra.js
@@ -5,13 +5,14 @@
 //var graph = {A: {B: 5, C: 10}, B: {A: 5, C: 4, D: 11}, C: {A: 10, B: 4, D: 5}, D: {B: 11, C: 5}};
 var graph = {A: {B: 10, C: 5}, B: {A: 10, D: 12, C: 8, E: 6}, C: {A: 5, B: 8, E: 12}, D: {B: 12, E: 5, F: 4}, E: {C: 12, B: 6, D: 5, F: 6}, F: {D: 4, E: 6}};
 var source = 'A';
-var destination = 'F';
+var destination = 'D';
 
 var settled = [];
 var dijkstra = [];
 var current_node = source;
 var graph_length = 0;
 var phi = 0;
+var path = [];
 
 // init dijsktra
 for(var key in graph) {
@@ -21,9 +22,10 @@ for(var key in graph) {
 
 dijkstra[current_node] = 0;
 settled.push(current_node);
+path.push(current_node);
 
 // start dijkstra
-while(settled.length < graph_length) {
+while(settled.length < graph_length && current_node !== destination) {
     
     var queue = [];
     var adjNodes = graph[current_node];
@@ -32,8 +34,14 @@ while(settled.length < graph_length) {
         
         // process only unsettled node
         if(settled.indexOf(key) === -1) {
+            
+            // mark for path
+            var changeTailPath = 0;
+            if(dijkstra[key] < phi + adjNodes[key])
+                changeTailPath = 1;
+            
             dijkstra[key] = Math.min(dijkstra[key], phi + adjNodes[key]);
-            queue.push({key: key, val: dijkstra[key]});
+            queue.push({key: key, val: dijkstra[key], changeTailPath: changeTailPath});
         }
     }
     
@@ -48,6 +56,18 @@ while(settled.length < graph_length) {
         }
         
         current_node = queue[idxMin].key;
+        
+        /* 
+            if the minimum cost is not from previous node then replace 
+            the node on path, else append current node
+        */
+        
+        if(queue[idxMin].changeTailPath) {
+            path[path.length - 1] = current_node;
+        } else {
+            path.push(current_node);
+        }
+        
         phi = dijkstra[current_node];
         settled.push(current_node);
     }
@@ -55,4 +75,5 @@ while(settled.length < graph_length) {
 
 console.log(dijkstra);
 console.log("optimum length from " + source + " to " + destination + ": " + dijkstra[destination]);
+console.log("optimum path: " + path.join(' -> '));
 
